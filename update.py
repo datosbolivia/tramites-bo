523c158fd4ea9d33e7e6f247e9720280a0696e3f
Mauricio Foronda <josemauricioforonda@gmail.com>
2025-09-16 10:46:01 -0400

    fix detectarAdiciones

--- update.py
+++ update.py
@@ -185,7 +185,7 @@ def detectarAdiciones(df1, df2, timestamp):
 
     # El formato de la bitácora
     def formatear(df, evento, timestamp):
-        n = df[["id", "entidad.nombre", "nombre"]].copy()
+        n = df[["id", "entidad", "nombre"]].copy()
         n.columns = ["id", "entidad", "nombre"]
         n.insert(0, "tipo", evento)
         n.insert(0, "timestamp", timestamp)
